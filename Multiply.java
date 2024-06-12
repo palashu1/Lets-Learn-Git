622c4407d2aeba809ca30886c928e8b0b49a85d6
Ashutosh Pal <ashutoshpal3179@gmail.com>
2024-06-12 10:38:19 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,5 @@
+public static void main(string[] args){
+    int a=10;
+    int b=20;
+    System.out.printIn("the product is"+a*b);
+}
---
+++ multi.java
@@ -0,0 +1,5 @@
+public static void main(string[] args){
+    int a=3;
+    int b=4;
+    System.out.printIn(a*b);
+}
